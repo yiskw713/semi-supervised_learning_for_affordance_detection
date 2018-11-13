51ac8f8e73e5099f61b83111b9dbc3fe5d46b095
Yuchi Ishikawa <yuchi@yuchi.dhcp.hakozaki.ibm.com>
2018-11-13 16:02:43 +0900

    fix mean, std

--- train_semi_unet.py
+++ train_semi_unet.py
@@ -48,7 +48,7 @@ def init_weights(m):
     elif isinstance(m, nn.BatchNorm2d):
         nn.init.constant_(m.weight, 1)
         if m.bias is not None:
-            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
 
 
 ''' training '''

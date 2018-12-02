3d51d641284cd0b88863c47b8d715e082e4e4e41
Yuchi Ishikawa <yuchi@yiskw.local>
2018-12-03 07:56:35 +0900

    fix FCDiscriminator.py

--- models/FCdiscriminator.py
+++ models/FCdiscriminator.py
@@ -35,7 +35,7 @@ class FCDiscriminator(nn.Module):
     def __init__(self, config, ndf = 64):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(config.n_classes + self.config.in_channel, ndf, kernel_size=4, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(self.onfig.n_classes + self.config.in_channel, ndf, kernel_size=4, stride=2, padding=1)
         self.conv2 = nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1)
         self.conv3 = nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1)
         self.conv4 = nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=2, padding=1)
